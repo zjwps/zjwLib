fa0d42013fbb2552f6df469459a41c5fd2e50b81
zjw <zjwps@163.com>
2017-09-01 16:30:26 +0800

    测试 C#6的 async await

---
+++ Assets/Temp/TestAwaitStep.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+/// <summary>
+/// 测试 C#6的 async await
+/// </summary>
+public class TestAwaitStep : MonoBehaviour
+{
+    Step1 mStep1;
+    async void Start (){
+        mStep1 = new Step1();
+        Debug.Log("wait start");
+        await Tesetfn1();
+        Debug.Log("wait over");
+    }
+
+    private int count = 2;
+    private void Update()
+    {
+        if (mStep1 != null)
+        {
+            if (count > 0)
+            {
+                Debug.Log(count);
+                count--;
+                return;
+            }
+
+            mStep1.Complete();
+            mStep1 = null;
+        }
+    }
+    AwaitStep Tesetfn1(){
+        return mStep1;
+    }
+    public class Step1:AwaitStep{
+        public Step1()
+        {
+            //OnCompleted(null);
+        }
+
+        
+    }
+}
---
+++ Assets/ZjwLib/AwaitStep.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+
+
+public class AwaitStep :INotifyCompletion
+{
+    private Action continuation;
+    private bool isCompleted;
+    public bool IsCompleted { get { return isCompleted; } }
+    public virtual void OnCompleted(Action continuation)
+    {
+        this.continuation = continuation;
+    }
+    public void Complete()
+    {
+        isCompleted = true;
+        continuation();
+    }
+    public void GetResult() { }
+    public AwaitStep GetAwaiter() => this;
+}
